ae95fc03c7e58429fc94101a02f7ede2cc8ba73c
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-09-05 13:59:02 +0700

    ✨ feat(lang): Add wrench mode translations and logic
    This commit adds new translations to en_US.lang for wrench modes,
    including default, offset, and offset + side. It also introduces
    the necessary Java code to handle these new wrench modes and their
    corresponding functionality.

--- src/main/java/ruiseki/integrateddynamics/item/ItemWrench.java
+++ src/main/java/ruiseki/integrateddynamics/item/ItemWrench.java
@@ -1,9 +1,15 @@
 package ruiseki.integrateddynamics.item;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -20,10 +26,8 @@ import ruiseki.okcore.modcompat.capabilities.DefaultCapabilityProvider;
  */
 public class ItemWrench extends ItemBase {
 
-    /**
-     * Make a new item instance.
-     *
-     */
+    private static final Map<String, Mode> NAMED_MODES = new HashMap<String, Mode>();
+
     public ItemWrench() {
         super();
     }
@@ -39,15 +43,163 @@ public class ItemWrench extends ItemBase {
     }
 
     @Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+        if (player.isSneaking() && !world.isRemote) {
+            incrementMode(itemStack);
+            player.addChatMessage(
+                new ChatComponentTranslation(
+                    "item.items.integrateddynamics.wrench.mode",
+                    new ChatComponentTranslation(getMode(itemStack).getLabel())));
+            return itemStack;
+        }
+        return super.onItemRightClick(itemStack, world, player);
+    }
+
+    @Override
     public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
         float hitX, float hitY, float hitZ) {
-        Block block = world.getBlock(x, y, z);
-        if (!world.isRemote || player.isSneaking()) {
-            return false;
-        } else if (block.rotateBlock(world, x, y, z, ForgeDirection.getOrientation(side))) {
-            player.swingItem();
-            return true;
+        if (player != null && player.isSneaking()) {
+            switch (getMode(stack)) {
+                case OFFSET: {
+                    NBTTagCompound tag = getOrCreateTag(stack);
+                    tag.setInteger("posX", x);
+                    tag.setInteger("posY", y);
+                    tag.setInteger("posZ", z);
+                    if (!world.isRemote) {
+                        player.addChatMessage(
+                            new ChatComponentTranslation(
+                                "item.items.integrateddynamics.wrench.mode.offset.saved",
+                                x + ", " + y + ", " + z));
+                    }
+                    return true;
+                }
+                case OFFSET_SIDE: {
+                    NBTTagCompound tag = getOrCreateTag(stack);
+                    tag.setInteger("posX", x);
+                    tag.setInteger("posY", y);
+                    tag.setInteger("posZ", z);
+                    tag.setInteger("side", side);
+                    if (!world.isRemote) {
+                        player.addChatMessage(
+                            new ChatComponentTranslation(
+                                "item.items.integrateddynamics.wrench.mode.offset_side.saved",
+                                x + ", " + y + ", " + z,
+                                ForgeDirection.getOrientation(side)
+                                    .name()));
+                    }
+                    return true;
+                }
+                case DEFAULT: {
+                    // Xử lý kiểm tra Cable hoặc bỏ qua trên client
+                    break;
+                }
+            }
+        } else {
+            Mode mode = getMode(stack);
+            if (mode == Mode.DEFAULT) {
+                Block block = world.getBlock(x, y, z);
+                if (block.rotateBlock(world, x, y, z, ForgeDirection.getOrientation(side))) {
+                    player.swingItem();
+                    return true;
+                }
+            }
         }
-        return true;
+
+        return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
+    }
+
+    public Mode getMode(ItemStack itemStack) {
+        if (itemStack.hasTagCompound()) {
+            Mode mode = NAMED_MODES.get(
+                itemStack.getTagCompound()
+                    .getString("mode"));
+            if (mode != null) {
+                return mode;
+            }
+        }
+        return Mode.DEFAULT;
+    }
+
+    public void setMode(ItemStack itemStack, Mode mode) {
+        getOrCreateTag(itemStack).setString("mode", mode.getName());
+    }
+
+    public void incrementMode(ItemStack itemStack) {
+        Mode mode = getMode(itemStack);
+        int modeId = mode.ordinal();
+        Mode nextMode = Mode.values()[(modeId + 1) % Mode.values().length];
+        setMode(itemStack, nextMode);
+
+        if (itemStack.hasTagCompound()) {
+            NBTTagCompound tag = itemStack.getTagCompound();
+            tag.removeTag("posX");
+            tag.removeTag("posY");
+            tag.removeTag("posZ");
+            tag.removeTag("side");
+        }
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
+        super.addInformation(itemStack, player, list, par4);
+
+        Mode mode = getMode(itemStack);
+        list.add(
+            new ChatComponentTranslation(
+                "item.items.integrateddynamics.wrench.mode",
+                new ChatComponentTranslation(mode.getLabel())).getUnformattedText());
+
+        if (itemStack.hasTagCompound()) {
+            NBTTagCompound tag = itemStack.getTagCompound();
+            if (tag.hasKey("posX")) {
+                String posStr = tag.getInteger("posX") + ", " + tag.getInteger("posY") + ", " + tag.getInteger("posZ");
+                list.add(
+                    EnumChatFormatting.GRAY
+                        + new ChatComponentTranslation("item.items.integrateddynamics.wrench.mode.offset.pos", posStr)
+                            .getUnformattedText());
+            }
+            if (tag.hasKey("side")) {
+                list.add(
+                    EnumChatFormatting.GRAY + new ChatComponentTranslation(
+                        "item.items.integrateddynamics.wrench.mode.offset_side.side",
+                        ForgeDirection.getOrientation(tag.getInteger("side"))
+                            .name()).getUnformattedText());
+            }
+        }
+        list.add(
+            EnumChatFormatting.GRAY.toString() + EnumChatFormatting.ITALIC
+                + new ChatComponentTranslation(mode.getLabel() + ".info").getUnformattedText());
+    }
+
+    private NBTTagCompound getOrCreateTag(ItemStack stack) {
+        if (!stack.hasTagCompound()) {
+            stack.setTagCompound(new NBTTagCompound());
+        }
+        return stack.getTagCompound();
+    }
+
+    public enum Mode {
+
+        DEFAULT("integrateddynamics:default", "item.items.integrateddynamics.wrench.mode.default.name"),
+        OFFSET("integrateddynamics:offset", "item.items.integrateddynamics.wrench.mode.offset.name"),
+        OFFSET_SIDE("integrateddynamics:offset_side", "item.items.integrateddynamics.wrench.mode.offset_side.name");
+
+        private final String name;
+        private final String label;
+
+        Mode(String name, String label) {
+            this.name = name;
+            this.label = label;
+            NAMED_MODES.put(name, this);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getLabel() {
+            return label;
+        }
     }
 }
