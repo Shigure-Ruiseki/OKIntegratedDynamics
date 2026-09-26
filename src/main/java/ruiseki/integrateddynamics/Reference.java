756e6c288da3ed0bc42137a9d5a2c25d9ce366f9
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-09-26 19:59:32 +0700

    [chore](deps): Remove Et Futurum Requiem dependency [fix](deps): Update okcore dependency version format

--- src/main/java/ruiseki/integrateddynamics/Reference.java
+++ src/main/java/ruiseki/integrateddynamics/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
     public static final String MOD_ID = Tags.MOD_ID;
     public static final String MOD_NAME = Tags.MOD_NAME;
     public static final String MOD_VERSION = Tags.VERSION;
-    public static final String MOD_DEPENDENCIES = "required-after:okcore[26.09.26.0,);"
+    public static final String MOD_DEPENDENCIES = "required-after:okcore@[26.09.26.0,);"
         + "required-after:commoncapabilities;"
         + "required-after:gtnhlib;";
     public static final String VERSION_URL = "";
